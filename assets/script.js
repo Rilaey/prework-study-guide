2d8b69bd0c33489a52f1ae7055d0a7b0633b0409
Riley Newhart <riley@Rileys-MBP.lan>
2022-08-20 17:17:58 -0400

    added JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,26 @@
-/*var topic = "HTML";
+var topics = ["HTML", "CSS", "Git", "JavaScript"]
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}*/
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+}  
 
 /*var shapes = ["triangle", "square", "pentagon", "circle"];
 
@@ -18,8 +28,10 @@ for(var x = 0; x < shapes.length; x++) { //we use 0 because the array starts wit
     console.log(shapes[x]);
 }*/
 
-var topics = ["HTML", "CSS", "Git", "JavaScript"]
+console.log("Here are the topics we learned through the prework:")
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
+listTopics();
+
+console.log("Which topic should we study first?")
+
+selectTopic();
